a15b8f7c030ae46122cb2a7a7dd6d07be98db347
Zachary Melamed <zach@Zacharys-MacBook-Pro-3.local>
2021-12-11 01:15:14 -0500

    read and print request

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection (mut stream: TcpStream){
+    let mut buffer = [0;1024];
+
+    stream.read(&mut buffer).unwrap();
+    println!(
+        "Reguests: {}",
+        String::from_utf8_lossy(&buffer[..])
+    );
 }
